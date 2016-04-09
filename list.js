2599686d9d290938ac41667cb65de87c4069f1b7
jonmcgill <jon.d.mcgill@gmail.com>
2016-04-09 16:31:50 -0400

    list module's skeleton

---
+++ list.js
@@ -0,0 +1,152 @@
+/*
+ * list.js
+ *
+ * An immutable linked list data structure and a library
+ * of accompanying functions.
+ *
+ */
+
+// See the CommonJS Modules spec for a description of
+// JavaScript modules: http://www.commonjs.org/specs/modules/1.0/
+
+///////////// EMPTY LIST ///////////////
+
+var EMPTY = {}
+
+////////////// LIST CONSTRUCTOR ////////////////
+
+function List(x, tail) {
+   this.x = x;
+   this.tail = tail;
+   Object.freeze(this);
+}
+
+function isList(o) {
+   return o === EMPTY
+          || ('object' === typeof o
+              && Object.getPrototypeOf(o) === List.prototype);
+}
+
+/////////// cons ////////////////
+
+/* Append x to the front of the list.
+ */
+EMPTY.cons = function(x) { return new List(x, this); };
+List.prototype.cons = EMPTY.cons;
+
+/////////// eq //////////////////
+
+EMPTY.eq = function(l2) { return l2 === EMPTY; };
+List.prototype.eq = function(l2) {
+   if (!isList(l2)) return false;
+   var x_eq = isList(this.x) ? this.x.eq(l2.x) : this.x === l2.x;
+   return x_eq && this.tail.eq(l2.tail);
+}
+
+/////////// toString ////////////
+
+EMPTY.toString = function() { return "()"; };
+EMPTY.inspect = EMPTY.toString;
+
+List.prototype.toString = function() {
+   var node = this;
+   var s = "(";
+   while (node != EMPTY) {
+      if (node !== this) s += ", ";
+      s += node.x.toString();
+      node = node.tail;
+   }
+   return s + ")";
+}
+List.prototype.inspect = List.prototype.toString
+
+//////////// first //////////////
+
+/* Returns the first element of the list, or null if the list is empty. */
+EMPTY.first = function() {
+   // TODO
+};
+List.prototype.first = function() {
+   // TODO
+};
+
+//////////// rest ////////////////
+
+/* Returns the list with the first element removed. When called
+ * on the empty list, returns the empty list.
+ */
+EMPTY.rest = function() {
+   // TODO
+}
+List.prototype.rest = function() {
+   // TODO
+};
+
+//////////// map //////////////////
+
+/* Returns the list produced by calling f on each
+ * element of the list.
+ */
+EMPTY.map = function(f) {
+   // TODO
+}
+List.prototype.map = function(f) {
+   // TODO
+}
+
+//////////// reduce ///////////////
+
+/* Reduce f over the list.
+ *
+ * If the list is empty, returns initial.
+ * Otherwise, calls f with arguments initial and the first
+ * list element,  then calls f with the result and the next
+ * list element, and so on for all elements. Returns the
+ * result of the final invocation of f.
+ *
+ * Examples:
+ *   var add = function(a,b) { return a + b; };
+ *
+ *   0 === list([]).reduce(add, 0)
+ *   1 === list([1]).reduce(add, 0)
+ *   10 === list([1,2,3,4]).reduce(add, 0)
+ */
+EMPTY.reduce = function(f, initial) {
+   // TODO
+}
+List.prototype.reduce = function(f, initial) {
+   // TODO
+}
+
+////////////// reverse //////////////
+
+/* Return the reverse of the list.
+ */
+EMPTY.reverse = function() {
+   // TODO
+}
+
+List.prototype.reverse = function() {
+   // TODO
+}
+
+///////////// TOP-LEVEL FUNCTIONS ////////////
+
+/* Accept a variable number of arguments, returning them as a list.
+ *
+ * Examples:
+ *   list([]) // the empty list
+ *   list([1, 2, 3]) // the list (1,2,3)
+ */
+function list(items) {
+   // TODO
+}
+
+// Make sure EMPTY is immutable!
+Object.freeze(EMPTY);
+
+module.exports = {
+   EMPTY: EMPTY,
+   isList: isList,
+   list: list
+};
--- test/list_tests.js
+++ test/list_tests.js
@@ -1,1 +1,67 @@
 var assert = require('assert');
+var l = require('../list');
+
+describe('empty lists', function() {
+   it('should override toString', function() {
+      assert("()" === l.EMPTY.toString());
+   });
+   it('should be a list', function() {
+      assert(l.isList(l.EMPTY));
+   });
+   it('should equal itself', function() {
+      assert(l.EMPTY.eq(l.EMPTY));
+   });
+   it('should not be modifiable', function() {
+      l.EMPTY.foo = 1;
+      assert(undefined === l.EMPTY.foo);
+   });
+   it('has no first element', function() {
+      assert(undefined === l.EMPTY.first());
+   });
+   it('has an empty remainder', function() {
+      assert(l.EMPTY.rest() === l.EMPTY);
+   });
+});
+
+describe('cons', function () {
+   it('should produce a list', function() {
+      assert(l.isList(l.EMPTY.cons(1)));
+   });
+   it('should not produce an empty list', function() {
+      assert(l.EMPTY.cons(1) !== l.EMPTY);
+   });
+   it('should produce equal values when called with same args', function() {
+      assert(l.EMPTY.cons(1).eq(l.EMPTY.cons(1)));
+   });
+});
+
+describe('lists', function() {
+   it('should overide toString', function() {
+      assert("(1)" === l.EMPTY.cons(1).toString());
+      assert("(1, 2, 3)" === l.list([1, 2, 3]).toString());
+   });
+   it('should be equal when they have the same values', function() {
+      assert(l.list([1,2,3]).eq(l.list([1,2,3])));
+      assert(l.list([1,2,3]).eq(l.list([2,3]).cons(1)));
+   });
+   it('should not be equal when they have different values', function() {
+      assert(!l.list([1,2,3]).eq(l.list([1,2,4])));
+      assert(!l.list([1,2]).eq(l.list([1,2,3])));
+   });
+});
+
+describe('rest', function() {
+});
+
+describe('first', function() {
+});
+
+describe('map', function() {
+});
+
+describe('reduce', function() {
+});
+
+describe('reverse', function() {
+});
+
